607e9d7d9393ce311327c1d79778d9b9720214c4
sha1ksadiq <sadiq2001ali@gmail.com>
2022-04-26 00:10:30 +0530

    getMovies

---
+++ adbms-api/helper/config.js
@@ -0,0 +1,13 @@
+let config = function() {
+                return { 
+                                database: {
+                                host: "localhost",
+                                user: "root",
+                                password: "Madnessregent12*",
+                                database: "stream",
+                                multipleStatements: true
+                                }
+                };
+}
+
+module.exports = new config();
--- adbms-api/routes/movieRouter.js
+++ adbms-api/routes/movieRouter.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connection = require('../helper/db_conn');
-const auth = require('../helper/authorizeToken');
-
+const config = require('../helper/config');
+let connection = require('mysql2').createPool(config.database);
 const movieRouter = express.Router();
 
 movieRouter.use(bodyParser.json());
 
-movieRouter.route('/getByRating').get((req, res, next) => {
-                const offset = req.body.offset;
+movieRouter.route('/getMovies').get((req, res, next) => {
+    const offset = req.body.offset;
+    const queryType = req.body.query_type; //1 = Rating, 2 = genre
+    const query = req.body.query;
+    connection.getConnection((error, tempConnection)=>{
+                    if(error) {
+                                    console.log(error);
+                                    res.statusCode = 200;
+                                    return res.json({status: -1, message: "Something went wrong..."})
+                    }
+                    var sqlquery = `SELECT * FROM movies `
+                    switch(queryType) {
+                                    case 1: 
+                                        sqlquery += `ORDERBY RATING DESC `;
+                                    break;
+                                    
+                                    case 2:
+                                        sqlquery += `WHERE GENRE = ? `;
+                                    break;
+                    }
+                    sqlquery += `LIMIT 50 OFFSET ?; `;
+                    let fields = [query, offset];
+                    tempConnection.query(sqlquery,fields,(error, res, fields)=> {
+                        tempConnection.release();
+                        if(error) {
+                            console.log(error);
+                            res.statusCode = 200;
+                            return res.json({status: -1, message: "Something went wrong..."})
+                        }
+                        console.log(res);
+                    });
+                    }
+    });
+
                 // get by rating, ORDER DESC, OFFSET, LIMIT 50
 });
 
@@ -17,7 +48,3 @@ movieRouter.route('/getByGenre').get((req, res, next)=> {
                 // get by genre type as per req, LIMIT 50
 })
 
-movieRouter.route('/')
-.get(auth.authenticateToken(req, res, next) => {
-                
-})
