0872ce619094e92dbe2bd72b8eb4424aedf100b7
Basil Benedict Victor <basilbennedict@gmail.com>
2022-04-28 04:35:36 +0530

    Movie Cleanup

--- adbms-frontend/src/app/movies/movies.component.ts
+++ adbms-frontend/src/app/movies/movies.component.ts
@@ -3,26 +3,6 @@ import { StreamingService } from '../services/streaming.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { HttpParams } from '@angular/common/http';
 
-export interface PeriodicElement {
-  name: string;
-  num: number;
-  rating: number;
-  genre: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {num: 1, name: 'Hydrogen', rating: 1.0079, genre: 'H'},
-  {num: 2, name: 'Helium', rating: 4.0026, genre: 'He'},
-  {num: 3, name: 'Lithium', rating: 6.941, genre: 'Li'},
-  {num: 4, name: 'Beryllium', rating: 9.0122, genre: 'Be'},
-  {num: 5, name: 'Boron', rating: 10.811, genre: 'B'},
-  {num: 6, name: 'Carbon', rating: 12.0107, genre: 'C'},
-  {num: 7, name: 'Nitrogen', rating: 14.0067, genre: 'N'},
-  {num: 8, name: 'Oxygen', rating: 15.9994, genre: 'O'},
-  {num: 9, name: 'Fluorine', rating: 18.9984, genre: 'F'},
-  {num: 10, name: 'Neon', rating: 20.1797, genre: 'Ne'},
-];
-
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
