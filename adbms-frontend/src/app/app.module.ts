d8cc2a8d2f3d7845b59a25f44ada98ac3488c811
Basil Benedict Victor <basilbennedict@gmail.com>
2022-04-27 20:28:58 +0530

    Home Page

--- adbms-frontend/src/app/app.module.ts
+++ adbms-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -8,17 +9,22 @@ import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesComponent } from './movies/movies.component';
+import { TvComponent } from './tv/tv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MoviesComponent,
+    TvComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
